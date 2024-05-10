6f81931b01fde4f881db4d754477a3a410dd3bf8
root <mohamedmohsenali38@gmail.com>
2024-05-10 23:07:24 +0300

    adding backend file

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = var.bucket-name
     key    = var.bucket-path
     region = var.region
+    dynamodb_table = var.dynamodb-table-name // locking state file for preventing concurrent access for the same state file  
   }
 }
